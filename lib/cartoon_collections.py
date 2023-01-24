8ba440d24d3d6ec7ec42b87a191b86a9b321c38f
Mustafa Binalhag <mbin828@gmail.com>
2023-01-23 20:18:42 -0500

    lab done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,27 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarfs):
+    for i,dwarf in enumerate(dwarfs):
+        print(f"{i+1}. {dwarf}")
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(strings):
+    return [string.capitalize() + "!" for string in strings]
     pass
 
-def long_planeteer_calls():
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4 :
+            return True
+    return False
     pass
 
-def find_the_cheese():
+def find_the_cheese(arr):
+    cheeses = {
+        "cheddar": True,
+        "gouda": True,
+        "camembert": True,
+    }
+    for item in arr:
+        if item in cheeses:
+            return item
+    return None
     pass
